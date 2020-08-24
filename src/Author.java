5361f3780d517e3976684d80abc83b8364155508
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-08-24 14:30:25 -0500

    exercises

--- src/Author.java
+++ src/Author.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private  final List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
@@ -20,6 +20,7 @@ public class Author extends Person {
         return books;
     }
 
+    @Deprecated
     public List<String> publishedBooks() {
         return books;
     }
@@ -28,7 +29,7 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
+
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
